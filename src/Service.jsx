037866cc349a7df849b72bec763c401f6534bec8
Emmanuel <Kmaxtaxx@gmail.com>
2025-02-15 23:08:28 +0100

    UPD

--- src/Service.jsx
+++ src/Service.jsx
@@ -6,22 +6,22 @@ function MyServices() {
     <>
       <div className="bg-[#0c0d08] h-[151vh]  w-screen flex justify-center items-center ">
         <div className="h-[131vh]  w-full gap-3  flex-col items-center justify-center  flex   ">
-          <div className="flex flex-col  border border-solid   lg:h-[19vh] w-[80vw] lg:w-[68vw]">
-            <div className="flex flex-col items-center  border-[purple] border-[10px] border-solid  lg:h-[19vh] gap-4  lg:gap-8  w-full">
+          <div className="flex flex-col    lg:h-[19vh] w-[80vw] lg:w-[68vw]">
+            <div className="flex flex-col items-center   lg:h-[19vh] gap-4  lg:gap-8  w-full">
               <div className=" border-[#4f4e4e] flex flex-row items-center justify-center  lg:h-[5.4vh] border-solid   border-[2.5px] w-[27vw] lg:w-[10vw] rounded-[5vw] lg:rounded-[2vw]">
                 
                 <p className=" dongle-regular text-[27px] lg:text-[30px] text-[#9fe30e] leading-tight">
                   Services
                 </p>
               </div>
-              <div className=" flex flex-row  border border-solid  items-center justify-center text-center  w-full lg:w-[40vw] leading-tight h-[7vh]  ">
+              <div className=" flex flex-row   items-center justify-center text-center  w-full lg:w-[40vw] leading-tight h-[7vh]  ">
                 <h2 className="dongle-regular text-[#dddbdb]  text-[35px] lg:text-[58px]">
                   My Special Services
                 </h2>
               </div>
             </div>
           </div>
-          <div className="lg:w-full  w-[97vw] gap-6   border border-solid  h-[105vh]">
+          <div className="lg:w-full  w-screen gap-6    h-[105vh]">
             <MyServiceTitled numberList=" 01 ." serviceType="Web Development" />
             <MyServiceTitled
               numberList=" 02 ."
